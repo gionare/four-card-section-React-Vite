377285f611a9d0c1a098efc47334fe7b9502bfb1
Giorgi Nareklishvili <38813634+gionare@users.noreply.github.com>
2024-07-05 09:53:17 -0400

    add cound down function

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,11 +1,20 @@
 import "../shared/container.css";
 import CalculatorImage from "/images/icon-calculator.svg";
+import { useState } from "react";
 
 export default function Calculator() {
+  const [count, setCount] = useState(10);
+
+  function handleChange() {
+    setCount((prevCount) => (prevCount > 0 ? count - 1 : 0));
+  }
+
   return (
     <div className="box blue">
       <h2>Calculator</h2>
       <p>Uses data from past projects to provide better delivery estimates</p>
+      <h1>{count}</h1>
+      <button onClick={handleChange}>Click</button>
       <img src={CalculatorImage} alt="calculator image" />
     </div>
   );
