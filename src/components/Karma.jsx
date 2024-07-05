6b0180d71a00688c2bcf97733de414722458a3b0
Giorgi Nareklishvili <38813634+gionare@users.noreply.github.com>
2024-07-05 13:00:48 -0400

    add cound down function

--- src/components/Karma.jsx
+++ src/components/Karma.jsx
@@ -1,12 +1,27 @@
 import "../shared/container.css";
 import KarmaImage from "/images/icon-karma.svg";
+import BulbImage from "/images/bulb.jpeg";
+import { useState } from "react";
 
 export default function Karma() {
+  const [light, setLight] = useState(false);
+
+  function handleChange() {
+    setLight(true);
+  }
+
   return (
     <div className="box yellow">
       <h2>Karma</h2>
       <p>Regularly evaluates our talent to ensure quality</p>
-      <img src={KarmaImage} alt="karma image" />
+      <button onClick={handleChange}>Click</button>
+      <div>
+        {!light ? (
+          <img src={KarmaImage} alt="karma image" />
+        ) : (
+          <img width={80} src={BulbImage} alt="bulb image" />
+        )}
+      </div>
     </div>
   );
 }
